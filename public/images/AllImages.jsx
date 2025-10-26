20b7557b147e94b443b97921a7230f3fced73812
nrshakib <nrshakib007@gmail.com>
2025-10-26 15:31:58 +0600

    Added About Us

--- public/images/AllImages.jsx
+++ public/images/AllImages.jsx
@@ -3,9 +3,25 @@ import slider02 from "./HeroImage/slider02.jpg";
 import slider03 from "./HeroImage/slider03.jpg";
 import slider04 from "./HeroImage/slider04.jpg";
 
+import aboutUs01 from "./aboutUs01.png";
+import aboutUs02 from "./aboutUs02.png";
+
+import aboutIcon01 from "./icons/icon-about-trusted-1.svg.png";
+import aboutIcon02 from "./icons/icon-about-trusted-2.svg.png";
+
 export const heroSlider = {
   slider01: slider01,
   slider02: slider02,
   slider03: slider03,
   slider04: slider04,
+};
+
+export const aboutUsImages = {
+  aboutUs01: aboutUs01,
+  aboutUs02: aboutUs02,
+};
+
+export const aboutUsIcons = {
+  aboutIcon01: aboutIcon01,
+  aboutIcon02: aboutIcon02,
 };
