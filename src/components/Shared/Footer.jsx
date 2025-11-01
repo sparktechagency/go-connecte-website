4d2c2c798d31d3693291f0321538fc8df142d6da
nrshakib <nrshakib007@gmail.com>
2025-11-01 11:14:10 +0600

    Added FAQ Page

--- src/components/Shared/Footer.jsx
+++ src/components/Shared/Footer.jsx
@@ -81,7 +81,7 @@ export default function Footer() {
             </li>
             <li>
               <Link
-                href="/faq"
+                href="/frequently-asked-questions"
                 className="hover:text-[#00AEA8] transition-colors inline-block"
               >
                 Frequently Asked Questions
