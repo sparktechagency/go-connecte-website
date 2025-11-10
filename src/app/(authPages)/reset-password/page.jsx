8fc09a7c72d56b5268a395dc66bda4c7a462e713
nrshakib <nrshakib007@gmail.com>
2025-11-10 15:05:04 +0600

    Added Auth Pages

--- src/app/(authPages)/reset-password/page.jsx
+++ src/app/(authPages)/reset-password/page.jsx
@@ -16,45 +16,57 @@ import { IoIosEyeOff, IoMdEye } from "react-icons/io";
 import { toast } from "sonner";
 
 export default function ResetPassword() {
+  const { user, loading, error, logIn } = useLogIn();
   const [email, setEmail] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
   const router = useRouter();
+
+  const handleShowNewPassword = () => setShowPassword((prev) => !prev);
+  const handleShowConfirmPassword = () =>
+    setShowConfirmPassword((prev) => !prev);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if (!email) {
-      toast.error("Email is required");
-    } else if (!/\S+@\S+\.\S+/.test(email)) {
-      toast.error("Please enter a valid email address");
+    if (!newPassword || !confirmPassword) {
+      toast.error("Password is required");
+      return;
     }
 
-    try {
-      console.log(email);
-      router.push("/");
-    } catch (error) {
-      toast.error("Something went wrong. Please try again.");
+    if (newPassword !== confirmPassword) {
+      toast.error("Passwords do not match");
+      return;
     }
+
+    if (newPassword.length < 6 || confirmPassword.length < 6) {
+      toast.error("Password must be at least 6 characters");
+      return;
+    }
+    toast.success("Password reset successful!");
+
+    // Navigate to the sign-in page
+
+    router.push("/sign-in");
   };
 
   return (
     <div className="bg-linear-to-b from-[#FBFFFF] to-[#D6F9F8] min-h-[85vh] flex justify-center items-center p-4">
-      <div className="flex flex-col gap-4 sm:gap-8 bg-white shadow-md rounded-lg w-full max-w-md px-3 sm:px-8 py-4 sm:py-12">
-        <div className="flex flex-col gap-3 text-center">
-          <p className="text-center text-lg sm:text-3xl font-bold text-[#1A1D25]">
-            Reset Password
-          </p>
-          <p className="text-[#737373] text-xs sm:text-base">
-            Enter the email associated with your account to reset your password
-          </p>
-        </div>
+      <div className="flex flex-col gap-2 sm:gap-4 bg-white shadow-md rounded-lg w-full max-w-md px-3 sm:px-8 py-4 sm:py-12">
+        <p className="text-center text-lg sm:text-3xl font-bold text-[#1A1D25]">
+          Set New Password
+        </p>
 
         <form onSubmit={handleSubmit} className="flex flex-col gap-3">
           <TextField
-            label="Email"
+            label="Enter New Password"
             variant="outlined"
+            type={showPassword ? "text" : "password"}
             fullWidth
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            value={newPassword}
+            onChange={(e) => setNewPassword(e.target.value)}
             InputProps={{
               sx: {
                 height: {
@@ -97,10 +109,110 @@ export default function ResetPassword() {
               },
               "& .MuiOutlinedInput-input": {
                 padding: {
-                  xs: "8px 14px",
-                  md: "10px 14px",
-                  lg: "14px 14px",
+                  xs: "12px 14px",
+                  sm: "14px 14px",
+                  md: "16px 14px",
+                  lg: "18px 14px",
                 },
+              },
+            }}
+            slotProps={{
+              input: {
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label={
+                        showPassword ? "Hide password" : "Show password"
+                      }
+                      onClick={handleShowNewPassword}
+                      edge="end"
+                    >
+                      {showPassword ? (
+                        <IoIosEyeOff className="text-[#00AEA8]" />
+                      ) : (
+                        <IoMdEye className="text-[#00AEA8]" />
+                      )}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              },
+            }}
+          />
+          <TextField
+            label="Confirm New Password"
+            variant="outlined"
+            type={showConfirmPassword ? "text" : "password"}
+            fullWidth
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            InputProps={{
+              sx: {
+                height: {
+                  xs: "44px",
+                  sm: "48px",
+                  md: "52px",
+                  lg: "56px",
+                },
+              },
+            }}
+            InputLabelProps={{
+              sx: {
+                fontSize: {
+                  xs: "0.875rem",
+                  sm: "0.9375rem",
+                  md: "1rem",
+                },
+                transform: {
+                  xs: "translate(14px, 12px) scale(1)",
+                  sm: "translate(14px, 14px) scale(1)",
+                  md: "translate(14px, 16px) scale(1)",
+                  lg: "translate(14px, 18px) scale(1)",
+                },
+                "&.MuiInputLabel-shrink": {
+                  transform: "translate(14px, -9px) scale(0.75)",
+                },
+              },
+            }}
+            sx={{
+              "& .MuiOutlinedInput-root": {
+                "&:hover fieldset": {
+                  borderColor: "#00AEA8",
+                },
+                "&.Mui-focused fieldset": {
+                  borderColor: "#00AEA8",
+                },
+              },
+              "& .MuiInputLabel-root.Mui-focused": {
+                color: "#00AEA8",
+              },
+              "& .MuiOutlinedInput-input": {
+                padding: {
+                  xs: "12px 14px",
+                  sm: "14px 14px",
+                  md: "16px 14px",
+                  lg: "18px 14px",
+                },
+              },
+            }}
+            slotProps={{
+              input: {
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label={
+                        showConfirmPassword ? "Hide password" : "Show password"
+                      }
+                      onClick={handleShowConfirmPassword}
+                      edge="end"
+                    >
+                      {showConfirmPassword ? (
+                        <IoIosEyeOff className="text-[#00AEA8]" />
+                      ) : (
+                        <IoMdEye className="text-[#00AEA8]" />
+                      )}
+                    </IconButton>
+                  </InputAdornment>
+                ),
               },
             }}
           />
@@ -115,7 +227,7 @@ export default function ResetPassword() {
               padding: {
                 xs: "6px 16px",
                 sm: "10px 20px",
-                md: "10px 24px",
+                md: "12px 24px",
               },
               fontSize: {
                 xs: "0.875rem",
@@ -124,14 +236,18 @@ export default function ResetPassword() {
               },
               borderRadius: "8px",
               width: "100%",
-
+              minHeight: {
+                xs: "40px",
+                sm: "48px",
+                md: "52px",
+              },
               "&:hover": {
                 backgroundColor: "#007D74",
               },
             }}
             type="submit"
           >
-            Send OTP
+            Set Password
           </Button>
         </form>
       </div>
